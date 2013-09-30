7cfca8a0f408454aa9cd2d1b80145efd843eb0c0
Kaven <kavendb85@gmail.com>
2013-09-30 18:01:21 +0800

    添加资源映射文件配置

--- ypm.js
+++ ypm.js
@@ -4,112 +4,110 @@ fis.cli.name = "ypm";
 fis.cli.info = fis.util.readJSON(__dirname + '/package.json');
 
 fis.config.merge({
-    roadmap : {
-        path : [
+    roadmap: {
+        path: [
             {
-                //一级同名组件，可以引用短路径，比如sea-modules/jquery/juqery.js
+                //一级同名组件，可以引用短路径，比如modules/jquery/juqery.js
                 //直接引用为var $ = require('jquery');
-                reg : /^\/modules\/([^\/]+)\/\1\.(js|coffee|less|css)$/i,
+                reg: /^\/modules\/([^\/]+)\/\1\.(js|coffee|less|css)$/i,
                 //是组件化的，会被jswrapper包装
-                isMod : true,
+                isMod: true,
                 //less和css文件会做csssprite处理
-                useSprite : true,
+                useSprite: true,
                 //id为文件夹名
-                id : '$1'
+                id: '$1'
             },
             {
-                //sea-modules目录下的其他文件
-                reg : /^\/modules\/(.*)\.(js|coffee|less|css)$/i,
+                //modules目录下的其他文件
+                reg: /^\/modules\/(.*)\.(js|coffee|less|css)$/i,
                 //是组件化的，会被jswrapper包装
-                isMod : true,
+                isMod: true,
                 //less和css文件会做csssprite处理
-                useSprite : true,
-                //id是去掉sea-modules和.js后缀中间的部分
-                id : '$1'
+                useSprite: true,
+                //id是去掉modules和.js后缀中间的部分
+                id: '$1'
             },
             {
                 //.mixin.less后缀的文件
-                reg : /\.mixin\.less$/,
+                reg: /\.mixin\.less$/,
                 //仅当做函数调用，不发布
-                release : false
+                release: false
             },
             {
                 //其他js、css、coffee、less文件
-                reg : /\.(js|coffee|css|less)$/,
+                reg: /\.(js|coffee|css|less)$/,
                 //less和css文件会做csssprite处理
-                useSprite : true,
+                useSprite: true,
                 //不要放到js资源表里
-                useMap : false
+                useMap: false
             },
             {
                 //readme文件，不要发布
-                reg : /\/readme.md$/i,
-                release : false
+                reg: /\/readme.md$/i,
+                release: false
             },
             {
                 //前端模板
-                reg : '**.tmpl',
+                reg: '**.tmpl',
                 //当做类html文件处理，可以识别<img src="xxx"/>等资源定位标识
-                isHtmlLike : true,
+                isHtmlLike: true,
                 //只是内嵌，不用发布
-                release : false
+                release: false
             }
         ],
-        ext : {
+        ext: {
             //less输出为css文件
-            less : 'css',
+            less: 'css',
             //coffee输出为js文件
-            coffee : 'js'
+            coffee: 'js'
         }
     },
-    modules : {//fis插件配置
-        parser : {
+    modules: {//fis插件配置
+        parser: {
             //.tmpl后缀的文件使用fis-parser-utc插件编译
-            tmpl : 'utc',
+            tmpl: 'utc',
             //.coffee后缀的文件使用fis-parser-coffee-script插件编译
-            coffee : 'coffee-script',
+            coffee: 'coffee-script',
             //.less后缀的文件使用fis-parser-less插件编译
-            less : 'less'
+            less: 'less'
         },
-		optimizer : {
-            js : 'uglify-js',
-            css : 'clean-css',
-            png : 'png-compressor',
-			html:'html-compress'
+        optimizer: {
+            js: 'uglify-js',
+            css: 'clean-css',
+            png: 'png-compressor',
+            html: 'html-compress'
         },
-        lint : {
-            js : 'jshint'
-        }
+        lint: {
+            js: 'jshint'
+        },
+		postprocessor : {
+            js : 'jswrapper, require-async'
+        },
+        postpackager : 'modjs'
     },
-    settings : {
-        parser : {
-            'coffee-script' : {
+    settings: {
+        parser: {
+            'coffee-script': {
                 //不用coffee-script包装作用域
-                bare : true
+                bare: true
             }
         },
-        lint : {
-            jshint : {
+        lint: {
+            jshint: {
                 //排除对lib和jquery、backbone、underscore的检查
-                ignored : [ 'lib/**', /jquery|underscore/i ],
+                ignored: [ 'lib/**', /jquery|underscore/i ],
                 //使用中文报错
-                i18n : 'zh-CN'
+                i18n: 'zh-CN'
             }
         },
         postprocessor : {
-            js : 'jswrapper, require-async'
-        },
-		postpackager : {
-            modjs : {
-                subpath : 'pkg/map.js'
+            jswrapper : {
+                type : 'amd'
             }
         },
-        optimizer : {
-            'uglify-js' : {
-                mangle : {
-                    //不要压缩require关键字，否则seajs会识别不了require
-                    except : [ 'require' ]
-                }
+        postpackager: {
+            modjs: {
+                subpath: 'pkg/map.js'
             }
         }
     }
